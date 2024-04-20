73da442f419025775edc9fe101349aa7e9d9153b
Thiago <thiagocaffaro223@gmail.com>
2024-04-19 21:34:55 -0300

    *

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useRef, useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const audioRef = useRef();
+  const imageRef = useRef();
+  const [imageSrc, setImageSrc] = useState("");
+
+  function playAudio(){
+    audioRef.current.play();
+  }
+  const stopAudio = () => {
+    audioRef.current.pause(); // Pausa o áudio
+    audioRef.current.currentTime = 0; // Volta o áudio para o início
+  };
+
+
+  const showImage = (direction) =>{
+    if (direction == "left"){
+      setImageSrc("/fichaAgente/images/left.gif")
+      imageRef.current.style.height = "400px";
+    }
+    else{
+      setImageSrc("/fichaAgente/images/right.jpeg")
+      imageRef.current.style.height = "400px";
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div id='mainContainer'>
+      <div id='header'>
+        <h1>INFORMAÇÕES DO AGENTE</h1>
+        <audio ref={audioRef} src="/fichaAgente/audios/morse.mp3" />
+        <div id='audioBtn'>
+          <img src="/fichaAgente/icons/play.png" onClick={playAudio}/>
+          <img src="/fichaagente/icons/stop.png" onClick={stopAudio}/>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div id='main'>
+        <div id='buttons'>
+          <button className='choiceBtn' id='leftBtn' onClick={() => showImage('left')}>
+            <img src="/fichaAgente/icons/file.png" />
+          </button>
+          <button className='choiceBtn' id='rightBtn' onClick={() => showImage('right')} >
+            <img src="/fichaagente/icons/file.png"/>
+          </button>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div id='theImage' >
+        <img src={imageSrc} ref={imageRef}/>
+      </div>
+    </div>
   )
 }
 
